7bab62f54ec84cce2bfb77608937ce5386594b0b
Estebanm1812 <EstebanMendoza02@hotmail.com>
2021-10-30 18:07:23 -0500

    problems need to be solved

--- src/model/FIBA.java
+++ src/model/FIBA.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import collections.AVL;
 
 public class FIBA {
 
 	ArrayList<Player> allPlayers;
+	
+	ArrayList<Player> changeTree;
 	
 	private AVL<Player> pointsTree;
 	
@@ -21,11 +24,31 @@ public class FIBA {
 	
 	private AVL<Player> stealsTree;
 	
+	private Comparator<Player> comparePoints;
+	
+	private Comparator<Player> compareBounces;
+	
+	private Comparator<Player> compareSteals;
+	
+	private Comparator<Player> compareBlocks;
+	
+	private Comparator<Player> compareAssistences;
 	
 	public FIBA() {
 		
+		//Player tmp = new Player("name",00,"Team",00,00,00,00,00);
+		comparePoints = Comparator.comparing((Player player)->player.getPointsPerGame());
+		compareBounces = Comparator.comparing((Player player)->player.getBouncesPerGame());
+		compareSteals = Comparator.comparing((Player player)->player.getStealsPerGame());
+		compareBlocks = Comparator.comparing((Player player)->player.getBlocksPerGame());
+		compareAssistences = Comparator.comparing((Player player)-> player.getAssistencesPerGame());
+		
 		allPlayers = new ArrayList<Player>();
-		pointsTree = new AVL();
+		pointsTree = new AVL<Player>(comparePoints);
+		bouncesTree = new AVL<Player>(compareBounces);
+		blocksTree = new AVL<Player>(compareBlocks);
+		stealsTree = new AVL<Player>(compareSteals);
+		assistencesTree = new AVL<Player>(compareAssistences);
 	}
 	
 	
@@ -45,6 +68,7 @@ public class FIBA {
 				
 			}else {
 			allPlayers.add(tmp);
+			addPlayerToAllTrees(tmp);
 			}
 			line = br.readLine();
 			
@@ -52,11 +76,22 @@ public class FIBA {
 		br.close();
 		
 	}
+	/*
 	public void addPlayerToMainTree() {
 		
 	}
-
-
+	 */
+	public void addPlayerToAllTrees(Player player) {
+		
+		pointsTree.add(player);
+		bouncesTree.add(player);
+		assistencesTree.add(player);
+		blocksTree.add(player);
+		stealsTree.add(player);
+		
+	}
+	
+	
 	public ArrayList<Player> getAllPlayers() {
 		return allPlayers;
 	}
